829e1de36a6b18febc83efc0e4c173044f0eb769
Dinghino <dinghino@gmail.com>
2021-04-27 11:36:23 +0200

    cleanup base_writer

--- scraper/components/writers/base_writer.py
+++ scraper/components/writers/base_writer.py
@@ -25,27 +25,3 @@ class Writer(abc.ABC):
             wr = csv.writer(csvfile, delimiter='|', quotechar="|", quoting=csv.QUOTE_MINIMAL)
             for line in data:
                 wr.writerow(line) 
-
-
-# ==================
-
-    # def get_path_from_settings(self):
-    #     path = self.settings.output_path
-    #     if utils.path_contains_filename(path):
-    #         split = self.settings.output_path.split('/')
-    #         path = '/'.join(split[:-1])
-        
-    #     if path[-1] is not '/':
-    #         path += "/"
-
-    #     return path
-
-    # def get_filename_from_settings(self, content):
-    #     if not utils.path_contains_filename(self.settings.output_path):
-    #         return self._create_filename(content)
-
-    #     return self.settings.output_path.split('/')[-1]
-
-    # def sanitize_ticker(self, ticker):
-    #     # TODO: Take the ticker string and clean up os-restricted characters like / ? and so on
-    #     return ticker.replace("/","-")
