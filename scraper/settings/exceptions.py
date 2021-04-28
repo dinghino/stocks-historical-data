dd082078f5f67df290b9a266dee2bddba2ab006e
Dinghino <dinghino@gmail.com>
2021-04-28 14:52:46 +0200

    update exceptions class relationships

--- scraper/settings/exceptions.py
+++ scraper/settings/exceptions.py
@@ -1,29 +1,30 @@
+from scraper.settings import constants
 
 class SettingsException(ValueError):
-
     def __str__(self):
         return self.message
 
 class DateException(SettingsException):
-    def __init__(self, datestr, field, *args):
-
-        valid = ", ".join(valid_date_formats)
+    def __init__(self, datestr, field, *args, **kwargs):
+        valid = ", ".join(constants.VALID_DATES_FORMAT)
         self.message = "Could not parse {0} for '{1}'. Valid formats are: {2}".format(
             datestr, field, valid)
-        # if args: self.message = args[0]
-        # else: self.message = "There was an error while setting a date"
 
 class OutputTypeException(SettingsException):
-    def __init__(self, val, *args):
-        self.message = "Provided value '{}' is not valid. should be one of '{}'".format(
-            val, ", ".join(Settings.OUTPUT_TYPE.VALID))
-    
+    def __init__(self, val, *args, **kwargs):
+        self.message = "OUTPUT value '{}' is invalid. should be one of '{}'".format(
+            val, ", ".join(constants.OUTPUT_TYPE.VALID))
 
 class SourceException(SettingsException):
-    def __init__(self, val, *args):
-        self.message = "Provided value '{}' is not valid. should be one of '{}'".format(
-            val, ", ".join(Settings.SOURCES.VALID))
+    def __init__(self, val, *args, **kwargs):
+        self.message = "SOURCE value '{}' is invalid. should be one of '{}'".format(
+            val, ", ".join(constants.SOURCES.VALID))
 
-class MissingFile(Exception):
-    def __str__(self, val, *args):
-        return 'Settings file not found at {}'.format(val)
+class DialectException(SettingsException):
+    def __init__(self, val, *args, **kwargs):
+        self.message = "CSV DIALECT value '{}' is invalid. should be one of '{}'".format(
+            val, ", ".join(constants.CSV_OUT_DIALECTS.VALID))
+
+class MissingFile(SettingsException):
+    def __init__(self, val, *args, **kwargs):
+        self.message = 'Settings FILE not found at {}'.format(val)
