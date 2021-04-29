a9499c8bc394954749a21ac0cbda2ea43bf0a5de
Dinghino <dinghino@gmail.com>
2021-04-29 23:12:30 +0200

    tests.constants cleanup

--- tests/mocks/constants.py
+++ tests/mocks/constants.py
@@ -2,12 +2,12 @@ import os
 from scraper.settings.constants import SOURCES
 
 
-MOCK_PATHS    = os.path.dirname(os.path.abspath(__file__))
-SETTINGS_PATH = os.path.join(MOCK_PATHS, "options.json")
-TEMP_TO_FILE  = os.path.join(MOCK_PATHS, "temp.json")
-
-EXPECTED_DIR  = os.path.join(MOCK_PATHS, 'expected')
-SOURCES_DIR   = os.path.join(MOCK_PATHS, 'source')
+MOCKS_PATHS    = os.path.dirname(os.path.abspath(__file__))
+SETTINGS_PATH = os.path.join(MOCKS_PATHS, "options.json")
+TEMP_JSON_FILE= os.path.join(MOCKS_PATHS, "temp.json")
+TEMP_CSV_FILE = os.path.join(MOCKS_PATHS, "temp.csv")
+EXPECTED_DIR  = os.path.join(MOCKS_PATHS, 'expected')
+SOURCES_DIR   = os.path.join(MOCKS_PATHS, 'source')
 
 TARGET_URLS = {}
 TARGET_URLS[SOURCES.FINRA_SHORTS] = [
@@ -33,32 +33,3 @@ DATES = {
     "start": "",
     "end": "",
 }
-
-
-
-
-
-# class FakeDataItem:
-#     def __init__(self, expected, source_fname, source_addr_base):
-#         self.expected_fname = expected
-#         self.source_fname = source_fname
-#         self.source_addr= source_addr_base + source_fname
-
-
-# finra_shorts_1 = FakeDataItem(
-#     expected="CNMSshvol20210427.txt",
-#     source_fname="CNMSshvol20210427.txt",
-#     source_addr_base="http://regsho.finra.org/",
-# )
-# sec_ftd_1 = FakeDataItem(
-#     expected="cnsfails202103a.txt",
-#     source_fname="cnsfails202103a.zip",
-#     source_addr_base="https://www.sec.gov/files/data/fails-deliver-data/",
-# )
-
-# class FakeData:
-#     items = []
-#     def add(self, item):
-#         self.items.append(item)
-#     def get():
-#         pass
