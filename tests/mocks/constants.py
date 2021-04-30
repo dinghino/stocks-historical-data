b3fe39f1375484d3d866b0839860a1ce9b5fab54
Dinghino <dinghino@gmail.com>
2021-04-30 23:25:21 +0200

    lint tests

--- tests/mocks/constants.py
+++ tests/mocks/constants.py
@@ -2,12 +2,12 @@ import os
 from scraper.settings.constants import SOURCES
 
 
-MOCKS_PATHS    = os.path.dirname(os.path.abspath(__file__))
+MOCKS_PATHS = os.path.dirname(os.path.abspath(__file__))
 SETTINGS_PATH = os.path.join(MOCKS_PATHS, "options.json")
-TEMP_JSON_FILE= os.path.join(MOCKS_PATHS, "temp.json")
+TEMP_JSON_FILE = os.path.join(MOCKS_PATHS, "temp.json")
 TEMP_CSV_FILE = os.path.join(MOCKS_PATHS, "temp.csv")
-EXPECTED_DIR  = os.path.join(MOCKS_PATHS, 'expected')
-SOURCES_DIR   = os.path.join(MOCKS_PATHS, 'source')
+EXPECTED_DIR = os.path.join(MOCKS_PATHS, 'expected')
+SOURCES_DIR = os.path.join(MOCKS_PATHS, 'source')
 
 TARGET_URLS = {}
 TARGET_URLS[SOURCES.FINRA_SHORTS] = [
@@ -21,13 +21,12 @@ TARGET_URLS[SOURCES.SEC_FTD] = [
 DATA_FILES = {}
 DATA_FILES[SOURCES.FINRA_SHORTS] = {
     'expected': ["CNMSshvol20210427.txt"],
-    'source' : ["CNMSshvol20210427.txt"],
+    'source': ["CNMSshvol20210427.txt"],
 }
 DATA_FILES[SOURCES.SEC_FTD] = {
-    'expected': ["cnsfails202104a.txt","cnsfails202104b.txt"],
-    'source': ["cnsfails202104a.zip","cnsfails202104b.zip"],
+    'expected': ["cnsfails202104a.txt", "cnsfails202104b.txt"],
+    'source': ["cnsfails202104a.zip", "cnsfails202104b.zip"],
 }
-
 
 DATES = {
     "start": "",
