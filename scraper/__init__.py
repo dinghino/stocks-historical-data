31aea82d638e1f566e00f901eecbefac8a66bae7
Dinghino <dinghino@gmail.com>
2021-04-30 15:44:27 +0200

    move initial component registration in cli app
    This allows a clean slate when importing the package
    update tests accordingly
    
    TODO: Note in readme for package usage

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -5,15 +5,6 @@ from scraper.stocks import App
 from scraper import components
 from scraper import utils
 
-
-from scraper.components import fetchers, parsers, manager, writers
-from scraper.settings.constants import SOURCES, OUTPUT_TYPE
-
-manager.register_handler(SOURCES.FINRA_SHORTS, fetchers.Finra, parsers.Finra)
-manager.register_handler(SOURCES.SEC_FTD, fetchers.SecFtd, parsers.SecFtd)
-manager.register_writer(OUTPUT_TYPE.SINGLE_FILE, writers.SingleFile)
-manager.register_writer(OUTPUT_TYPE.SINGLE_TICKER, writers.MultiFile)
-
 # Root dir for scraper
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
