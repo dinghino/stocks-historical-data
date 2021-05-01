ec5a4fcbeb2dc4d83a7f771517b49a3201f741be
Dinghino <dinghino@gmail.com>
2021-05-01 18:31:45 +0200

    remove utils register dialects function has been moved inside manager so it's useless

--- scraper/utils.py
+++ scraper/utils.py
@@ -6,17 +6,3 @@ def path_contains_filename(path):
     if path[-4:] in ['.csv', '.txt']:
         return True
     return False
-
-
-# def register_custom_csv_dialects():
-#     csv.register_dialect(
-#         constants.CSV_OUT_DIALECTS.DEFAULT,
-#         delimiter="|",
-#         quoting=csv.QUOTE_MINIMAL
-#     )
-
-
-# TODO: Replace original function, this is just for testing purpose
-def register_custom_csv_dialects(data):
-    for name, args in data:
-        csv.register_dialect(name, **args)
