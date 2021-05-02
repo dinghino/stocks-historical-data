34f1727218041c22deb72298b1f9ed9338fe7bca
Dinghino <dinghino@gmail.com>
2021-05-02 18:25:50 +0200

    refactor cli with components automation
    as long as components are created the right way they are
    auto added to the cli

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,7 +1,12 @@
 from cli import cli
+from cli.setup import setup
+from stonks.components import writers, handlers
 
 # ===========================================================================
 
 if __name__ == "__main__":
+
+    dialects = [("default", {"delimiter": "|"})]
+    setup(handlers, writers, dialects)
 
     cli.start()
