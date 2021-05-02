b254f2083757ef68cbee6c1bd639ffbef4e6bad2
Dinghino <dinghino@gmail.com>
2021-05-02 13:16:58 +0200

    rename stocks.py to app.py + definitions
    update gitignore for new structure (defaults)

---
+++ stonks/__init__.py
@@ -0,0 +1,4 @@
+from scraper import App, Settings                               # noqa
+from scraper.components.fetchers.base_fetcher import Fetcher    # noqa
+from scraper.components.parsers.base_parser import Parser       # noqa
+from scraper.components.writers.base_writer import Writer       # noqa
--- stonks/scraper/__init__.py
+++ stonks/scraper/__init__.py
@@ -1,9 +1,4 @@
-import os
-
 from scraper.settings import Settings   # noqa
-from scraper.stocks import App          # noqa
+from scraper.app import App          # noqa
 from scraper import components          # noqa
 from scraper import utils               # noqa
-
-# Root dir for scraper
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
