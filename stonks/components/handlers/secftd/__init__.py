bef339cd40cdadcd5cda06f39da1136bf915f898
Dinghino <dinghino@gmail.com>
2021-05-02 21:46:21 +0200

    noqa

--- stonks/components/handlers/secftd/__init__.py
+++ stonks/components/handlers/secftd/__init__.py
@@ -1,2 +1,2 @@
-from stonks.components.handlers.secftd.secftd_fetcher import SecFtdFetcher as Fetcher
-from stonks.components.handlers.secftd.secftd_parser import SecFtdParser as Parser
+from stonks.components.handlers.secftd.secftd_fetcher import SecFtdFetcher as Fetcher   # noqa
+from stonks.components.handlers.secftd.secftd_parser import SecFtdParser as Parser      # noqa
