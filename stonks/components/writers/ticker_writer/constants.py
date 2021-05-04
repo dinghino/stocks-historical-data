11e97e4a5841d5094c1a7bfb4b6f5853c964c7ec
Dinghino <dinghino@gmail.com>
2021-05-04 18:13:22 +0200

    implement custom formatting in components
    add meaningful descriptions

--- stonks/components/writers/ticker_writer/constants.py
+++ stonks/components/writers/ticker_writer/constants.py
@@ -1,3 +1,9 @@
 output_type = "INDIVIDUAL_TICKER_MULTIFILE_WRITER"
 friendly_name = "Individual Ticker files"
-description = "Outputs one file for each symbol/ticker"
+description = """
+Outputs one file for each symbol/ticker.
+
+When selecting this output type usually the symbol is removed from
+the data since each file contains only one symbol.
+
+"""  # noqa
