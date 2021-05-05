bce35cef002146c669d581f068815c74ef5144b3
Dinghino <dinghino@gmail.com>
2021-05-05 23:21:29 +0200

    WIP test changing dates w/ special on launch
    work in progress and to properly do it requires fixing the settings validation issue

--- launcher/launchers.py
+++ launcher/launchers.py
@@ -1,4 +1,5 @@
 import click
+from datetime import datetime, timedelta
 import cli
 import stonks
 from launcher import validators, utils
@@ -21,6 +22,8 @@ def main():
 
 
 FILE_HELP = "Path to a settings file to read and write to."
+DATE_HELP = "Override and replace the set {d} date."
+START_DATE_EXTRA = "You can use 'last' to set it to the day after the "
 
 
 @main.command('cli')
@@ -39,14 +42,22 @@ def run_interactive_cli(settings_file=None):
 @main.command('run')
 @click.option('-f', '--settings-file', callback=validators.settings_path,
               help=(FILE_HELP), required=True)
+@click.option('-s', '--start_date', help=DATE_HELP.format(d="start"))
+@click.option('-e', '--end_date', help=DATE_HELP.format(d="end"))
 @click.option('-p', '--show-progress', is_flag=True,
               help="Show the progress bar")
 @click.option('--debug', is_flag=True,
               help="Output debug information")
-def run_from_args(settings_file, show_progress, debug, *args, **kwargs):
+def run_from_args(
+      settings_file, start_date, end_date, show_progress,
+      debug, *args, **kwargs):
     """Perform a run using the given settings to load.
 
     Note that the file is required and has to be a valid configuration file.
+
+    When passing a date you can also use
+    'last' on the start date to previous end date + 1 day and
+    'today' on the end day to set it to the current date
     """
 
     def dbg(txt): debug and click.echo(txt)
@@ -58,20 +69,75 @@ def run_from_args(settings_file, show_progress, debug, *args, **kwargs):
 
     settings = stonks.Settings(settings_path=settings_file, debug=debug)
     app = stonks.App(settings, show_progress=show_progress, debug=debug)
-    init_ok = settings.init()
+    init_ok = settings.init()  # read the settings from file to modify it
+
+    # validation done forward of handling changes and stuff
+    init_ok = validate_settings(settings, settings_file, debug)
+
+    parse_date_args(start_date, end_date, settings)
 
     debug and cli.utils.print_current_options(settings)
 
+    # perform a last validation after updating the values
+    init_ok = validate_settings(settings, settings_file, debug)
+
+    if init_ok:
+        execute_run(app, settings, show_progress, prog)
+    else:
+        click.echo("There were issues loading the settings. Bailing.")
+
+
+def execute_run(app, settings, show_progress, proglog):
     if show_progress:
         cleaner = cli.utils.run_cleaner(
             settings, current_settings=False, clear_screen=False)
         cleaner()
 
-    if init_ok:
-        for _ in app.run():
-            pass  # do nothing, just iterate through the generator
-        show_progress and cleaner and cleaner()
+    for _ in app.run():
+        pass  # do nothing, just iterate through the generator
+    show_progress and cleaner and cleaner()
 
-        prog(utils.parse_path(settings.output_path))
-    else:
-        click.echo("There were issues loading the settings. Bailing.")
+    proglog(utils.parse_path(settings.output_path))
+    app.settings.to_file()
+
+
+def validate_settings(settings, path, debug):
+    # TODO: Replace with method inside settings
+    if not cli.utils.validate_settings(settings, debug):
+        # TODO: Proper logging please!
+        print(f"{utils.parse_path(path)} or"
+              " arguments had issues. please check.")
+        click.Abort()
+    return True
+
+
+def parse_start_date_arg(date, settings):
+    """Parse the start date including 'last' word to get a new file. """
+    if date == 'last':
+        return (settings.end_date + timedelta(days=1)).strftime('%Y-%m-%d')
+    return date
+
+
+def parse_end_date_arg(date, settings):
+    """Parse our end date including our 'today' word."""
+    if date == 'today':
+        return datetime.now().date().strftime('%Y-%m-%d')
+    return date
+
+
+def parse_date_args(start, end, settings):
+    # process end dates
+    try:
+        if start:
+            settings.start_date = parse_start_date_arg(start, settings)
+    except stonks.exceptions.DateException:
+        click.echo(
+            "Wrong date format passed for START DATE. use one of "
+            ', '.join(settings.VALID_DATES_FORMAT))
+    try:
+        if end:
+            settings.end_date = parse_end_date_arg(end, settings)
+    except stonks.exceptions.DateException:
+        click.echo(
+            "Wrong date format passed for END DATE. use one of "
+            ', '.join(settings.VALID_DATES_FORMAT))
