04058849972eb80ca41e201c33571ce5a9d8f004
daniele <dinghino@gmail.com>
2021-05-05 14:58:06 +0200

    move cli.init to stonks.init
    allows to properly initialize the manager with the app
    more semantically correct

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,44 +1,20 @@
 import os
 import click
 from cli import entry
-from stonks import Settings
-from stonks.components import manager, writers, handlers
+import stonks
 
 
 def launch():
-    if init():
-        start()
-    else:
-        echo_error()
+    if not stonks.init():
+        return echo_error()
 
-
-def init(handlers_module=None, writers_module=None, dialects=[]):
-    done = True
-    dialects = [('default', {'delimiter': '|'}), *dialects]
-    # register native components
-    done = done and manager.register_dialects_from_list(dialects)
-    done = done and manager.register_writers_from_module(writers)
-    done = done and manager.register_handlers_from_modules(handlers)
-
-    # process extra components provided on setup
-    if handlers_module:
-        done = done and manager.register_handlers_from_modules(handlers_module)
-    if writers_module:
-        done = done and manager.register_writers_from_module(writers_module)
-
-    return done
-
-
-def start():
     os.system('clear')
-
-    settings = Settings()
+    settings = stonks.Settings()
     if settings.init():
         click.echo("Settings loaded")
+        entry.run(settings)
     else:
         click.echo("There was an error Loading the settings")
-
-    entry.run(settings)
 
 
 def echo_error():
--- stonks/__init__.py
+++ stonks/__init__.py
@@ -1,6 +1,27 @@
 from stonks.app import App                                # noqa
-from stonks.components import manager                     # noqa
+from stonks.components import manager, handlers, writers  # noqa
 from stonks.settings import Settings                      # noqa
 from stonks.components.base_fetcher import FetcherBase    # noqa
 from stonks.components.base_parser import ParserBase      # noqa
 from stonks.components.base_writer import WriterBase      # noqa
+
+
+def init(handlers_module=None, writers_module=None, dialects=[]):
+    """Initialize the manager with the all the available modules, adding
+    the provided optional ones.
+    Returns True is everything went corretly
+    """
+    done = True
+    dialects = [('default', {'delimiter': '|'}), *dialects]
+    # register native components
+    done = done and manager.register_dialects_from_list(dialects)
+    done = done and manager.register_writers_from_module(writers)
+    done = done and manager.register_handlers_from_modules(handlers)
+
+    # process extra components provided on setup
+    if handlers_module:
+        done = done and manager.register_handlers_from_modules(handlers_module)
+    if writers_module:
+        done = done and manager.register_writers_from_module(writers_module)
+
+    return done
